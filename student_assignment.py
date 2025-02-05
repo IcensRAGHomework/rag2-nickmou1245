f5e472930a6e34c531537167d63a4e2ae30e7efc
Nick Mou <Nick_Mou@asus.com>
2025-02-05 09:13:59 +0800

    update hw02_1 and hw02_2 #1
    update regex pattern

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,37 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    pdf_loader = PyPDFLoader(q1_pdf)
+    ct_splitter = CharacterTextSplitter(chunk_overlap=0)
+    chunks = ct_splitter.split_documents(pdf_loader.load())
+
+    return chunks[len(chunks) - 1]
 
 def hw02_2(q2_pdf):
-    pass
+    pdf_loader = PyPDFLoader(q2_pdf)
+    pages = pdf_loader.load()
+
+    # 解決分頁內截斷問題
+    full_text = "\n".join([page.page_content for page in pages])
+
+    regex_pattern = r"第 (?:[1-9][0-9]|.*) (?:條|章)"
+    rct_splitter = RecursiveCharacterTextSplitter(
+        chunk_overlap=0,
+        chunk_size=20,
+        separators=[regex_pattern],
+        is_separator_regex=True)
+
+    rct_chunks = rct_splitter.split_text(full_text)
+
+    # debug print
+    #print(f"Total chunks: {len(rct_chunks)}")
+    #count = 0
+    #for chunk in rct_chunks:
+    #    count = count + 1
+    #    print(f"chunk:{count} ===============")
+    #    print(chunk[:200])
+
+    return len(rct_chunks)
+
+#print(hw02_1(q1_pdf))
+print(hw02_2(q2_pdf))
